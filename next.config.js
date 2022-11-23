09985f2ebc6d8bfaefadcd2e3f605e947d6d4a82
Keita Furuse <keitafuruse@KeitanoMacBook-Pro.local>
2022-11-23 15:13:41 +0900

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+  // 参考：https://zenn.dev/catnose99/scraps/661d77118aa2af
+});
+const withImages = require("next-images");
 
-module.exports = nextConfig
+module.exports = withImages(
+  withBundleAnalyzer({
+    pageExtensions: ["page.tsx"],
+    images: {
+      disableStaticImages: true,
+    },
+    eslint: {
+      dirs: ["*/*", "src/*"],
+    },
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback.fs = false;
+      }
+      return config;
+    },
+  })
+);
