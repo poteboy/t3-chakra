f84285fc2535524f4b382bb38cf38510d0144b72
poteboy <poteboy.dev@gmail.com>
2022-11-25 00:11:50 +0900

    move @types to dev dependencies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
 import Head from "next/head";
 import Image from "next/image";
-import { useQuery } from "@src/utils";
+import { useMutation } from "@src/utils";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { CreateUserInput, createUserSchema } from "@src/schema";
 
 export default function Home() {
+  const {} = useForm<CreateUserInput>({
+    resolver: zodResolver(createUserSchema),
+    mode: "onChange",
+  });
+
+  const { mutate } = useMutation(["users.register-user"]);
+
   return <></>;
 }
